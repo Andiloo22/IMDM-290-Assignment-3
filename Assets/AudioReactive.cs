1e4c887bf00b4237929f42b4c3ff1ab1610461a5
Andiloo22 <Andiloo22789@gmail.com>
2026-02-23 19:00:26 -0500

    2nd layer

--- Assets/AudioReactive.cs
+++ Assets/AudioReactive.cs
@@ -4,17 +4,27 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioReactive : MonoBehaviour
 {
-    GameObject[] spheres;
-    public Mesh flower;
-    static int numSphere = 10;
+    GameObject[] flowers;
+    public Mesh flowerMesh;
+    static int numFlowers = 10;
     float time = 0f;
     Vector3[] initPos;
     Vector3[] startPosition, endPosition;
     float lerpFraction; // Lerp point between 0~1
+
+    GameObject[] flowerShape;
+    Vector3[] flowerShapeStart;
+    Vector3[] flowerShapeEnd;
+    public Mesh flowerShapeMesh;
+
+    int numFlowerShape = 40;
+    int petals = 24;
+    float flowerRadius = 8f;
 
     //peak swirl effect
     GameObject[] swirl;
@@ -27,12 +37,9 @@ public class AudioReactive : MonoBehaviour
     float spectrum;
     float spectrum2;
     float timer;
-    float squareScale = 0.125f;
+    float FlowerScale = 0.125f;
     float random = 0f;
-    float green = 0.4f;
     float blue = 0.5f;
-    float orange = 0.06f;
-    float cubeCol;
     float backCol;
     float part2 = 24;
     float part3 = 48;
@@ -43,12 +50,12 @@ public class AudioReactive : MonoBehaviour
     void Start()
     {
         // Assign proper types and sizes to the variables.
-        spheres = new GameObject[numSphere];
-        initPos = new Vector3[numSphere]; // Start positions
-        startPosition = new Vector3[numSphere];
-        endPosition = new Vector3[numSphere];
+        flowers = new GameObject[numFlowers];
+        initPos = new Vector3[numFlowers]; // Start positions
+        startPosition = new Vector3[numFlowers];
+        endPosition = new Vector3[numFlowers];
         // Define target positions. Start = random, End = heart 
-        for (int i = 0; i < numSphere; i++)
+        for (int i = 0; i < numFlowers; i++)
         {
             // Random start positions
             float r = 5f;
@@ -56,29 +63,56 @@ public class AudioReactive : MonoBehaviour
 
             // Circular end position
             r = 3f;
-            endPosition[i] = new Vector3(r * Mathf.Sin(i * 2 * Mathf.PI / numSphere), r * Mathf.Cos(i * 2 * Mathf.PI / numSphere));
+            endPosition[i] = new Vector3(r * Mathf.Sin(i * 2 * Mathf.PI / numFlowers), r * Mathf.Cos(i * 2 * Mathf.PI / numFlowers));
         }
         // Let there be spheres..
-        for (int i = 0; i < numSphere; i++)
+        for (int i = 0; i < numFlowers; i++)
         {
             // Draw primitive elements:
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/GameObject.CreatePrimitive.html
-            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            flowers[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
             // Position
             initPos[i] = startPosition[i];
-            spheres[i].transform.position = initPos[i];
-            spheres[i].transform.localRotation = Quaternion.EulerAngles(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f));
-            spheres[i].transform.localScale = new Vector3(Random.Range(0.3f, 0.5f), Random.Range(0.3f, 0.5f), Random.Range(0.3f, 0.5f));
+            flowers[i].transform.position = initPos[i];
+            flowers[i].transform.localRotation = Quaternion.Euler(270f, 0f, 0f); ;
+            flowers[i].transform.localScale = new Vector3(Random.Range(0.3f, 0.5f), Random.Range(0.3f, 0.5f), Random.Range(0.3f, 0.5f));
             // Color
             // Get the renderer of the spheres and assign colors.
-            Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
+            Renderer sphereRenderer = flowers[i].GetComponent<Renderer>();
             // HSV color space: https://en.wikipedia.org/wiki/HSL_and_HSV
-            float hue = (float)i / numSphere; // Hue cycles through 0 to 1
+            float hue = (float)i / numFlowers; // Hue cycles through 0 to 1
             Color color = Color.HSVToRGB(hue, 1f, 1f); // Full saturation and brightness
             sphereRenderer.material.color = color;
-            MeshFilter cubeMesh = spheres[i].GetComponent<MeshFilter>();
-            cubeMesh.mesh = flower;
+            MeshFilter cubeMesh = flowers[i].GetComponent<MeshFilter>();
+            cubeMesh.mesh = flowerMesh;
+        }
+
+        flowerShape = new GameObject[numFlowerShape];
+        flowerShapeStart = new Vector3[numFlowerShape];
+        flowerShapeEnd = new Vector3[numFlowerShape];
+
+        for (int i = 0; i < numFlowerShape; i++)
+        {
+            float theta = i * 2 * Mathf.PI / numFlowerShape;
+
+            float r = flowerRadius * Mathf.Sin(petals * theta);
+
+            float x = r * Mathf.Cos(theta);
+            float y = r * Mathf.Sin(theta);
+
+            flowerShapeEnd[i] = new Vector3(x, y, 0f);
+            flowerShapeStart[i] = new Vector3(0f, 0f, 20f);
+
+            flowerShape[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            flowerShape[i].transform.position = flowerShapeStart[i];
+            flowerShape[i].transform.localRotation = Quaternion.Euler(270f, 0f, 0f); ;
+            flowerShape[i].transform.localScale = Vector3.one * 0.05f;
+
+            Renderer rend = flowerShape[i].GetComponent<Renderer>();
+            rend.material.color = Color.magenta;
+            MeshFilter flowerShapeFilter = flowerShape[i].GetComponent<MeshFilter>();
+            flowerShapeFilter.mesh = flowerShapeMesh;
         }
 
         //swirl p3?
@@ -122,7 +156,6 @@ public class AudioReactive : MonoBehaviour
 
         if (timer <= part2)
         {
-            cubeCol = green;
             backCol = blue;
             spectrum = AudioSpectrum.drum1;
             Debug.Log("part1");
@@ -136,13 +169,14 @@ public class AudioReactive : MonoBehaviour
         else if (timer <= part4)
         {
             UpdateSwirl(AudioSpectrum.vocal3); //at peak, change to swirl pattern
+            spectrum = AudioSpectrum.vocal3;
             //spectrum = AudioSpectrum.vocal3;
             Debug.Log("part3");
         }
         else if (timer <= part5)
         {
             //if (timer >= part3) UpdateSwirl(AudioSpectrum.vocal3); //at peak, change to swirl pattern
-            backCol = orange;
+            backCol = 0f;
             spectrum = AudioSpectrum.overall4;
             Debug.Log("part4");
         }
@@ -151,17 +185,21 @@ public class AudioReactive : MonoBehaviour
             HideSwirl(); //hide swirl objs
         }
 
+        spectrum = Mathf.Lerp(spectrum, spectrum, Time.deltaTime * .25f);
+        
+
         float spectrum2 = AudioSpectrum.bass;
-            // ***Here, we use audio Amplitude, where else do you want to use?
-            // Measure Time 
-            // Time.deltaTime = The interval in seconds from the last frame to the current one
-            // but what if time flows according to the music's amplitude?
-            //time += Time.deltaTime * AudioSpectrum.audioAmp;
-            time += Time.deltaTime * spectrum;
+        spectrum2 = Mathf.Lerp(spectrum2, spectrum2, Time.deltaTime * 0.25f);
+        // ***Here, we use audio Amplitude, where else do you want to use?
+        // Measure Time 
+        // Time.deltaTime = The interval in seconds from the last frame to the current one
+        // but what if time flows according to the music's amplitude?
+        //time += Time.deltaTime * AudioSpectrum.audioAmp;
+        time += Time.deltaTime * spectrum;
         Debug.Log(spectrum);
         Debug.Log(spectrum2 + "Spec 2");
         // what to update over time?
-        for (int i = 0; i < numSphere; i++)
+        for (int i = 0; i < numFlowers; i++)
         {
             // Lerp : Linearly interpolates between two points.
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
@@ -171,21 +209,34 @@ public class AudioReactive : MonoBehaviour
             lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f;
 
             // Lerp logic. Update position       
-            t = i * 2 * Mathf.PI / numSphere;
-            spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
-            float scale = squareScale + (spectrum);
-            spheres[i].transform.localScale = new Vector3(scale, squareScale, scale);
-            spheres[i].transform.Rotate(spectrum, spectrum, spectrum);
+            t = i * 2 * Mathf.PI / numFlowers;
+            flowers[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
+            float scale = FlowerScale + (spectrum);
+            flowers[i].transform.localScale = new Vector3(scale, FlowerScale, scale);
+            flowers[i].transform.Rotate(0f, spectrum, 0f);
 
             // Color Update over time
-            Renderer cubeRenderer = spheres[i].GetComponent<Renderer>();
-            float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(cubeCol, spectrum, 1f); // Full saturation and brightness
+            Renderer cubeRenderer = flowers[i].GetComponent<Renderer>();
+            float hue = (float)i / numFlowers; // Hue cycles through 0 to 1
+            Color color = Color.HSVToRGB(0.2f + (0.8f - 0.2f) * (Mathf.Clamp01(spectrum2)), 1f, 1f); // Full saturation and brightness
             cubeRenderer.material.color = color;
         }
 
+        float flowerLerp = Mathf.Sin(time * 0.5f) * 0.5f + spectrum2;
+
+        for (int i = 0; i < numFlowerShape; i++)
+        {
+            flowerShape[i].transform.position =
+                Vector3.Lerp(flowerShapeStart[i], flowerShapeEnd[i], flowerLerp);
+
+            float scale = FlowerScale + (spectrum2);
+            flowerShape[i].transform.localScale = new Vector3(scale, FlowerScale, scale);
+
+            flowerShape[i].transform.Rotate(0f, spectrum2, 0f);
+        }
+
         Camera camera = Camera.main;
-        camera.backgroundColor = Color.HSVToRGB(backCol, spectrum2, 1f);
+        camera.backgroundColor = Color.HSVToRGB(0.55f, 0.75f, 1f);
     }
 
     //manage pattern change
